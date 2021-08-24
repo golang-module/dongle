d2f3f7b708100abd5f71fd5bd5202712d1a1094f
gouguoyin <245629560@qq.com>
2021-08-24 14:14:35 +0800

    move errors to a separate file

--- errors.go
+++ errors.go
@@ -8,3 +8,18 @@ import (
 var invalidFileError = func(file string) error {
 	return fmt.Errorf("invalid file %q, please make sure the file exists", file)
 }
+
+// invalidEncodingModeError returns an invalid decoding mode error
+var invalidDecodingModeError = func(mode string) error {
+	return fmt.Errorf("invalid decoding mode %q", mode)
+}
+
+// invalidGroupModeError returns an invalid group mode error
+var invalidGroupModeError = func(mode string) error {
+	return fmt.Errorf("invalid group mode %q", mode)
+}
+
+// invalidPaddingModeError returns an invalid padding mode error
+var invalidPaddingModeError = func(mode string) error {
+	return fmt.Errorf("invalid padding mode %q", mode)
+}
