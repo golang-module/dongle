376ce55455b38e5697b6e7b16f7b4ba81947baff
gouguoyin <245629560@qq.com>
2022-05-19 16:38:57 +0800

    v0.1.0 update error message

--- errors.go
+++ errors.go
@@ -9,17 +9,25 @@ var invalidFileError = func(file string) error {
 	return fmt.Errorf("invalid file %q, please make sure the file exists", file)
 }
 
-// invalidEncodingModeError returns an invalid decoding mode error
-var invalidDecodingModeError = func(mode string) error {
-	return fmt.Errorf("invalid decoding mode %q", mode)
+// invalidKeyError returns an invalid key error
+var invalidKeyError = func(length int, size ...int) error {
+	if len(size) == 0 {
+		return fmt.Errorf("invalid key size %d, key must be 16, 24 or 32 bytes", length)
+	}
+	return fmt.Errorf("invalid key size %d, key must be %d bytes", length, size[0])
 }
 
-// invalidGroupModeError returns an invalid group mode error
-var invalidGroupModeError = func(mode string) error {
-	return fmt.Errorf("invalid group mode %q", mode)
+// overflowKeyError returns a overflow key error
+var overflowKeyError = func(length int) error {
+	return fmt.Errorf("invalid key size %d, key at least 1 byte and at most 256 bytes", length)
 }
 
-// invalidPaddingModeError returns an invalid padding mode error
-var invalidPaddingModeError = func(mode string) error {
-	return fmt.Errorf("invalid padding mode %q", mode)
+// invalidIVError returns an invalid IV error
+var invalidIVError = func(length int, size int) error {
+	return fmt.Errorf("invalid IV size %d, IV size must be %d", length, size)
+}
+
+// invalidModeOrPaddingError returns an invalid encryption mode or padding error
+var invalidModeOrPaddingError = func(mode, padding string) error {
+	return fmt.Errorf("invalid encryption mode %q or padding %q", mode, padding)
 }
