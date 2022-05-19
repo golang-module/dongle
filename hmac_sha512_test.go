b6e3660b76b9d007b8e95746e2c55b9b9413b1b2
gouguoyin <245629560@qq.com>
2022-05-19 16:35:01 +0800

    v0.1.0 add hmac-sha512 encryption

---
+++ hmac_sha512_test.go
@@ -0,0 +1,113 @@
+package dongle
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	hmacSha512Input, hmacSha512Key = "hello world", "dongle"
+	hmacSha512HexExpected          = "d971b790bbc2a4ac81062bbffac693c9c234bae176c8faf5e304dbdb153032a826f12353964b4a4fb87abecd2dc237638a630cbad54a6b94b1f6ef5d5e2835d1"
+	hmacSha512Base32Expected       = "3FY3PEF3YKSKZAIGFO77VRUTZHBDJOXBO3EPV5PDATN5WFJQGKUCN4JDKOLEWSSPXB5L5TJNYI3WHCTDBS5NKSTLSSY7N325LYUDLUI="
+	hmacSha512Base64Expected       = "2XG3kLvCpKyBBiu/+saTycI0uuF2yPr14wTb2xUwMqgm8SNTlktKT7h6vs0twjdjimMMutVKa5Sx9u9dXig10Q=="
+)
+
+func TestEncrypt_ByHmacSha512_FromStringToString(t *testing.T) {
+	assert := assert.New(t)
+
+	hexTests := []struct {
+		input1   string // 输入值1
+		input2   string // 输入值2
+		expected string // 期望值
+	}{
+		{"", "", ""},
+		{hmacSha512Input, hmacSha512Key, hmacSha512HexExpected},
+	}
+
+	for index, test := range hexTests {
+		e := Encrypt.FromString(test.input1).ByHmacSha512(test.input2)
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToHexString(), "Current test index is "+strconv.Itoa(index))
+	}
+
+	base32Tests := []struct {
+		input1   string // 输入值1
+		input2   string // 输入值2
+		expected string // 期望值
+	}{
+		{"", "", ""},
+		{hmacSha512Input, hmacSha512Key, hmacSha512Base32Expected},
+	}
+
+	for index, test := range base32Tests {
+		e := Encrypt.FromString(test.input1).ByHmacSha512(test.input2)
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToBase32String(), "Current test index is "+strconv.Itoa(index))
+	}
+
+	base64Tests := []struct {
+		input1   string // 输入值1
+		input2   string // 输入值2
+		expected string // 期望值
+	}{
+		{"", "", ""},
+		{hmacSha512Input, hmacSha512Key, hmacSha512Base64Expected},
+	}
+
+	for index, test := range base64Tests {
+		e := Encrypt.FromString(test.input1).ByHmacSha512(test.input2)
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToBase64String(), "Current test index is "+strconv.Itoa(index))
+	}
+}
+
+func TestEncrypt_ByHmacSha512_FromBytesToBytes(t *testing.T) {
+	assert := assert.New(t)
+
+	hexTests := []struct {
+		input1   []byte // 输入值1
+		input2   []byte // 输入值2
+		expected []byte // 期望值
+	}{
+		{[]byte(""), []byte(""), []byte("")},
+		{[]byte(hmacSha512Input), []byte(hmacSha512Key), []byte(hmacSha512HexExpected)},
+	}
+
+	for index, test := range hexTests {
+		e := Encrypt.FromBytes(test.input1).ByHmacSha512(test.input2)
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToHexBytes(), "Current test index is "+strconv.Itoa(index))
+	}
+
+	base32Tests := []struct {
+		input1   []byte // 输入值1
+		input2   []byte // 输入值2
+		expected []byte // 期望值
+	}{
+		{[]byte(""), []byte(""), []byte("")},
+		{[]byte(hmacSha512Input), []byte(hmacSha512Key), []byte(hmacSha512Base32Expected)},
+	}
+
+	for index, test := range base32Tests {
+		e := Encrypt.FromBytes(test.input1).ByHmacSha512(test.input2)
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToBase32Bytes(), "Current test index is "+strconv.Itoa(index))
+	}
+
+	base64Tests := []struct {
+		input1   []byte // 输入值1
+		input2   []byte // 输入值2
+		expected []byte // 期望值
+	}{
+		{[]byte(""), []byte(""), []byte("")},
+		{[]byte(hmacSha512Input), []byte(hmacSha512Key), []byte(hmacSha512Base64Expected)},
+	}
+
+	for index, test := range base64Tests {
+		e := Encrypt.FromBytes(test.input1).ByHmacSha512(test.input2)
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToBase64Bytes(), "Current test index is "+strconv.Itoa(index))
+	}
+}
