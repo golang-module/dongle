e640b598b55d2e14cebf4049a8fe2960cf9deeb2
gouguoyin <245629560@qq.com>
2022-05-19 16:31:30 +0800

    v0.1.0 add hmac-sha1 encryption

---
+++ hmac_sha1.go
@@ -0,0 +1,25 @@
+package dongle
+
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"hash"
+)
+
+// ByHmacSha1 encrypts by hmac use sha1.
+// 通过 hmac-sha1 加密
+func (e encrypt) ByHmacSha1(key interface{}) encrypt {
+	if len(e.src) == 0 {
+		return e
+	}
+	var mac hash.Hash
+	switch v := key.(type) {
+	case string:
+		mac = hmac.New(sha1.New, string2bytes(v))
+	case []byte:
+		mac = hmac.New(sha1.New, v)
+	}
+	mac.Write(e.src)
+	e.dst = mac.Sum(nil)
+	return e
+}
---
+++ hmac_sha1_test.go
@@ -0,0 +1,113 @@
+package dongle
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	hmacSha1Input, hmacSha1Key = "hello world", "dongle"
+	hmacSha1HexExpected        = "91c103ef93ba7420902b0d1bf0903251c94b4a62"
+	hmacSha1Base32Expected     = "SHAQH34TXJ2CBEBLBUN7BEBSKHEUWSTC"
+	hmacSha1Base64Expected     = "kcED75O6dCCQKw0b8JAyUclLSmI="
+)
+
+func TestEncrypt_ByHmacSha1_FromStringToString(t *testing.T) {
+	assert := assert.New(t)
+
+	hexTests := []struct {
+		input1   string // 输入值1
+		input2   string // 输入值2
+		expected string // 期望值
+	}{
+		{"", "", ""},
+		{hmacSha1Input, hmacSha1Key, hmacSha1HexExpected},
+	}
+
+	for index, test := range hexTests {
+		e := Encrypt.FromString(test.input1).ByHmacSha1(test.input2)
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToHexString(), "Current test index is "+strconv.Itoa(index))
+	}
+
+	base32Tests := []struct {
+		input1   string // 输入值1
+		input2   string // 输入值2
+		expected string // 期望值
+	}{
+		{"", "", ""},
+		{hmacSha1Input, hmacSha1Key, hmacSha1Base32Expected},
+	}
+
+	for index, test := range base32Tests {
+		e := Encrypt.FromString(test.input1).ByHmacSha1(test.input2)
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToBase32String(), "Current test index is "+strconv.Itoa(index))
+	}
+
+	base64Tests := []struct {
+		input1   string // 输入值1
+		input2   string // 输入值2
+		expected string // 期望值
+	}{
+		{"", "", ""},
+		{hmacSha1Input, hmacSha1Key, hmacSha1Base64Expected},
+	}
+
+	for index, test := range base64Tests {
+		e := Encrypt.FromString(test.input1).ByHmacSha1(test.input2)
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToBase64String(), "Current test index is "+strconv.Itoa(index))
+	}
+}
+
+func TestEncrypt_ByHmacSha1_FromBytesToBytes(t *testing.T) {
+	assert := assert.New(t)
+
+	hexTests := []struct {
+		input1   []byte // 输入值1
+		input2   []byte // 输入值2
+		expected []byte // 期望值
+	}{
+		{[]byte(""), []byte(""), []byte("")},
+		{[]byte(hmacSha1Input), []byte(hmacSha1Key), []byte(hmacSha1HexExpected)},
+	}
+
+	for index, test := range hexTests {
+		e := Encrypt.FromBytes(test.input1).ByHmacSha1(test.input2)
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToHexBytes(), "Current test index is "+strconv.Itoa(index))
+	}
+
+	base32Tests := []struct {
+		input1   []byte // 输入值1
+		input2   []byte // 输入值2
+		expected []byte // 期望值
+	}{
+		{[]byte(""), []byte(""), []byte("")},
+		{[]byte(hmacSha1Input), []byte(hmacSha1Key), []byte(hmacSha1Base32Expected)},
+	}
+
+	for index, test := range base32Tests {
+		e := Encrypt.FromBytes(test.input1).ByHmacSha1(test.input2)
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToBase32Bytes(), "Current test index is "+strconv.Itoa(index))
+	}
+
+	base64Tests := []struct {
+		input1   []byte // 输入值1
+		input2   []byte // 输入值2
+		expected []byte // 期望值
+	}{
+		{[]byte(""), []byte(""), []byte("")},
+		{[]byte(hmacSha1Input), []byte(hmacSha1Key), []byte(hmacSha1Base64Expected)},
+	}
+
+	for index, test := range base64Tests {
+		e := Encrypt.FromBytes(test.input1).ByHmacSha1(test.input2)
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToBase64Bytes(), "Current test index is "+strconv.Itoa(index))
+	}
+}
