59c0b958aa4ec8ed133b10867d93b2938da8e8e9
gouguoyin <245629560@qq.com>
2022-05-19 16:33:18 +0800

    v0.1.0 add hmac-sha256 encryption

---
+++ hmac_sha256.go
@@ -0,0 +1,25 @@
+package dongle
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"hash"
+)
+
+// ByHmacSha256 encrypts by hmac use sha256.
+// 通过 hmac-sha256 加密
+func (e encrypt) ByHmacSha256(key interface{}) encrypt {
+	if len(e.src) == 0 {
+		return e
+	}
+	var mac hash.Hash
+	switch v := key.(type) {
+	case string:
+		mac = hmac.New(sha256.New, string2bytes(v))
+	case []byte:
+		mac = hmac.New(sha256.New, v)
+	}
+	mac.Write(e.src)
+	e.dst = mac.Sum(nil)
+	return e
+}
