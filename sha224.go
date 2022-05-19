64fbffc198bcf95c824961cd934fb0d4985a6873
gouguoyin <245629560@qq.com>
2022-05-19 16:36:52 +0800

    v0.1.0 add sha224 encryption

--- sha224.go
+++ sha224.go
@@ -5,14 +5,12 @@ import (
 )
 
 // BySha224 encrypts by sha224.
+// 通过 sha224 加密
 func (e encrypt) BySha224() encrypt {
-	if e.Error != nil {
+	if len(e.src) == 0 {
 		return e
 	}
-	if len(e.input) == 0 {
-		return e
-	}
-	bytes := sha256.Sum224(e.input)
-	e.output = bytes[:]
+	bytes := sha256.Sum224(e.src)
+	e.dst = bytes[:]
 	return e
 }
--- sha224_test.go
+++ sha224_test.go
@@ -7,108 +7,102 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestEncrypt_BySha224_FromString_ToString(t *testing.T) {
+var (
+	sha224Input          = "hello world"
+	sha224HexExpected    = "2f05477fc24bb4faefd86517156dafdecec45b8ad3cf2522a563582b"
+	sha224Base32Expected = "F4CUO76CJO2PV36YMULRK3NP33HMIW4K2PHSKIVFMNMCW==="
+	sha224Base64Expected = "LwVHf8JLtPrv2GUXFW2v3s7EW4rTzyUipWNYKw=="
+)
+
+func TestEncrypt_BySha224_FromStringToString(t *testing.T) {
 	assert := assert.New(t)
 
-	tests := []struct {
-		input      string // 输入值
-		encodeMode string // 编码模式
-		expected   string // 期望值
+	hexTests := []struct {
+		input    string // 输入值
+		expected string // 期望值
 	}{
-		{"", "hex", ""},
-		{"", "base32", ""},
-		{"", "base64", ""},
-		{"", "xxx", ""},
-		{"hello world", "xxx", ""},
-		{"hello world", "hex", "2f05477fc24bb4faefd86517156dafdecec45b8ad3cf2522a563582b"},
-		{"hello world", "base32", "F4CUO76CJO2PV36YMULRK3NP33HMIW4K2PHSKIVFMNMCW==="},
-		{"hello world", "base64", "LwVHf8JLtPrv2GUXFW2v3s7EW4rTzyUipWNYKw=="},
+		{"", ""},
+		{sha224Input, sha224HexExpected},
 	}
 
-	for index, test := range tests {
+	for index, test := range hexTests {
 		e := Encrypt.FromString(test.input).BySha224()
 		assert.Nil(e.Error)
-		assert.Equal(test.expected, e.ToString(test.encodeMode), "Current test index is "+strconv.Itoa(index))
+		assert.Equal(test.expected, e.ToHexString(), "Current test index is "+strconv.Itoa(index))
 	}
-}
 
-func TestEncrypt_BySha224_FromString_ToBytes(t *testing.T) {
-	assert := assert.New(t)
-
-	tests := []struct {
-		input      string // 输入值
-		encodeMode string // 编码模式
-		expected   []byte // 期望值
+	base32Tests := []struct {
+		input    string // 输入值
+		expected string // 期望值
 	}{
-		{"", "hex", []byte{}},
-		{"", "base32", []byte{}},
-		{"", "base64", []byte{}},
-		{"", "xxx", []byte{}},
-		{"hello world", "xxx", []byte("")},
-		{"hello world", "hex", []byte("2f05477fc24bb4faefd86517156dafdecec45b8ad3cf2522a563582b")},
-		{"hello world", "base32", []byte("F4CUO76CJO2PV36YMULRK3NP33HMIW4K2PHSKIVFMNMCW===")},
-		{"hello world", "base64", []byte("LwVHf8JLtPrv2GUXFW2v3s7EW4rTzyUipWNYKw==")},
+		{"", ""},
+		{sha224Input, sha224Base32Expected},
 	}
 
-	for index, test := range tests {
+	for index, test := range base32Tests {
 		e := Encrypt.FromString(test.input).BySha224()
 		assert.Nil(e.Error)
-		assert.Equal(test.expected, e.ToBytes(test.encodeMode), "Current test index is "+strconv.Itoa(index))
+		assert.Equal(test.expected, e.ToBase32String(), "Current test index is "+strconv.Itoa(index))
+	}
+
+	base64Tests := []struct {
+		input    string // 输入值
+		expected string // 期望值
+	}{
+		{"", ""},
+		{sha224Input, sha224Base64Expected},
+	}
+
+	for index, test := range base64Tests {
+		e := Encrypt.FromString(test.input).BySha224()
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToBase64String(), "Current test index is "+strconv.Itoa(index))
 	}
 }
 
-func TestEncrypt_BySha224_FromBytes_ToString(t *testing.T) {
+func TestEncrypt_BySha224_FromBytesToBytes(t *testing.T) {
 	assert := assert.New(t)
 
-	tests := []struct {
-		input      []byte // 输入值
-		encodeMode string // 编码模式
-		expected   string // 期望值
+	hexTests := []struct {
+		input    []byte // 输入值
+		expected []byte // 期望值
 	}{
-		{[]byte(""), "hex", ""},
-		{[]byte(""), "base32", ""},
-		{[]byte(""), "base64", ""},
-		{[]byte(""), "xxx", ""},
-		{[]byte("hello world"), "xxx", ""},
-		{[]byte("hello world"), "hex", "2f05477fc24bb4faefd86517156dafdecec45b8ad3cf2522a563582b"},
-		{[]byte("hello world"), "base32", "F4CUO76CJO2PV36YMULRK3NP33HMIW4K2PHSKIVFMNMCW==="},
-		{[]byte("hello world"), "base64", "LwVHf8JLtPrv2GUXFW2v3s7EW4rTzyUipWNYKw=="},
+		{[]byte(""), []byte("")},
+		{[]byte(sha224Input), []byte(sha224HexExpected)},
 	}
 
-	for index, test := range tests {
+	for index, test := range hexTests {
 		e := Encrypt.FromBytes(test.input).BySha224()
 		assert.Nil(e.Error)
-		assert.Equal(test.expected, e.ToString(test.encodeMode), "Current test index is "+strconv.Itoa(index))
+		assert.Equal(test.expected, e.ToHexBytes(), "Current test index is "+strconv.Itoa(index))
 	}
-}
 
-func TestEncrypt_BySha224_FromBytes_ToBytes(t *testing.T) {
-	assert := assert.New(t)
-
-	tests := []struct {
-		input      []byte // 输入值
-		encodeMode string // 编码模式
-		expected   []byte // 期望值
+	base32Tests := []struct {
+		input    []byte // 输入值
+		expected []byte // 期望值
 	}{
-		{[]byte(""), "hex", []byte{}},
-		{[]byte(""), "base32", []byte{}},
-		{[]byte(""), "base64", []byte{}},
-		{[]byte(""), "xxx", []byte{}},
-		{[]byte("hello world"), "xxx", []byte("")},
-		{[]byte("hello world"), "hex", []byte("2f05477fc24bb4faefd86517156dafdecec45b8ad3cf2522a563582b")},
-		{[]byte("hello world"), "base32", []byte("F4CUO76CJO2PV36YMULRK3NP33HMIW4K2PHSKIVFMNMCW===")},
-		{[]byte("hello world"), "base64", []byte("LwVHf8JLtPrv2GUXFW2v3s7EW4rTzyUipWNYKw==")},
+		{[]byte(""), []byte("")},
+		{[]byte(sha224Input), []byte(sha224Base32Expected)},
 	}
 
-	for index, test := range tests {
+	for index, test := range base32Tests {
 		e := Encrypt.FromBytes(test.input).BySha224()
 		assert.Nil(e.Error)
-		assert.Equal(test.expected, e.ToBytes(test.encodeMode), "Current test index is "+strconv.Itoa(index))
+		assert.Equal(test.expected, e.ToBase32Bytes(), "Current test index is "+strconv.Itoa(index))
 	}
-}
 
-func TestError_BySha224_FromFile(t *testing.T) {
-	file := "./demo.txt"
-	e := Encrypt.FromFile(file).BySha224()
-	assert.Equal(t, invalidFileError(file), e.Error, "Should catch an exception")
+	base64Tests := []struct {
+		input    []byte // 输入值
+		expected []byte // 期望值
+	}{
+		{[]byte(""), []byte("")},
+		{[]byte(sha224Input), []byte(sha224Base64Expected)},
+	}
+
+	for index, test := range base64Tests {
+		e := Encrypt.FromBytes(test.input).BySha224()
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToBase64Bytes(), "Current test index is "+strconv.Itoa(index))
+	}
+
 }
