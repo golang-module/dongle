bdbc96d7a01828355c04773a3aa2e6a0c5c4b031
gouguoyin <245629560@qq.com>
2022-05-19 16:24:53 +0800

    v0.1.0 add url base64 encoding&decoding

---
+++ base64_url.go
@@ -0,0 +1,29 @@
+package dongle
+
+import (
+	"encoding/base64"
+)
+
+// ByBase64URL encodes by base64.
+// 通过 base64 对 url 编码
+func (e encode) ByBase64URL() encode {
+	if len(e.src) == 0 {
+		return e
+	}
+	buf := make([]byte, base64.URLEncoding.EncodedLen(len(e.src)))
+	base64.URLEncoding.Encode(buf, e.src)
+	e.dst = buf
+	return e
+}
+
+// ByBase64URL encodes by base64.
+// 通过 base64 对 url 解码
+func (d decode) ByBase64URL() decode {
+	if len(d.src) == 0 {
+		return d
+	}
+	buf := make([]byte, base64.URLEncoding.DecodedLen(len(d.src)))
+	n, err := base64.URLEncoding.Decode(buf, d.src)
+	d.dst, d.Error = buf[:n], err
+	return d
+}
---
+++ base64_url_test.go
@@ -0,0 +1,85 @@
+package dongle
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	base64UrlInput    = "www.gouguoyin.cn"
+	base64UrlExpected = "d3d3LmdvdWd1b3lpbi5jbg=="
+)
+
+func TestEncode_ByBase64URL_FromStringToString(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		input    string // 输入值
+		expected string // 期望值
+	}{
+		{"", ""},
+		{base64UrlInput, base64UrlExpected},
+	}
+
+	for index, test := range tests {
+		e := Encode.FromString(test.input).ByBase64URL()
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToString(), "Current test id is "+strconv.Itoa(index))
+	}
+}
+
+func TestDecode_ByBase64URL_FromStringToString(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		input    string // 输入值
+		expected string // 期望值
+	}{
+		{"", ""},
+		{base64UrlExpected, base64UrlInput},
+	}
+
+	for index, test := range tests {
+		d := Decode.FromString(test.input).ByBase64URL()
+		assert.Nil(d.Error)
+		assert.Equal(test.expected, d.ToString(), "Current test id is "+strconv.Itoa(index))
+	}
+}
+
+func TestEncode_ByBase64URL_FromBytesToBytes(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		input    []byte // 输入值
+		expected []byte // 期望值
+	}{
+		{[]byte(""), []byte("")},
+		{[]byte(base64UrlInput), []byte(base64UrlExpected)},
+	}
+
+	for index, test := range tests {
+		e := Encode.FromBytes(test.input).ByBase64URL()
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToBytes(), "Current test id is "+strconv.Itoa(index))
+	}
+}
+
+func TestDecode_ByBase64URL_FromBytesToBytes(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		input    []byte // 输入值
+		expected []byte // 期望值
+	}{
+		{[]byte(""), []byte("")},
+		{[]byte(base64UrlExpected), []byte(base64UrlInput)},
+	}
+
+	for index, test := range tests {
+		d := Decode.FromBytes(test.input).ByBase64URL()
+		assert.Nil(d.Error)
+		assert.Equal(test.expected, d.ToBytes(), "Current test id is "+strconv.Itoa(index))
+	}
+}
