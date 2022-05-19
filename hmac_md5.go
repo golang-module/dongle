3979f5a23c0f28a372ee30ad1a68285f5e1bcaf7
gouguoyin <245629560@qq.com>
2022-05-19 16:31:07 +0800

    v0.1.0 add hmac-md5 encryption

---
+++ hmac_md5.go
@@ -0,0 +1,25 @@
+package dongle
+
+import (
+	"crypto/hmac"
+	"crypto/md5"
+	"hash"
+)
+
+// ByHmacMd5 encrypts by md5.
+// 通过 hmac-md5 加密
+func (e encrypt) ByHmacMd5(key interface{}) encrypt {
+	if len(e.src) == 0 {
+		return e
+	}
+	var mac hash.Hash
+	switch v := key.(type) {
+	case string:
+		mac = hmac.New(md5.New, string2bytes(v))
+	case []byte:
+		mac = hmac.New(md5.New, v)
+	}
+	mac.Write(e.src)
+	e.dst = mac.Sum(nil)
+	return e
+}
---
+++ hmac_md5_test.go
@@ -0,0 +1,113 @@
+package dongle
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	hmacMd5Input, hmacMd5Key = "hello world", "dongle"
+	hmacMd5HexExpected       = "4790626a275f776956386e5a3ea7b726"
+	hmacMd5Base32Expected    = "I6IGE2RHL53WSVRYNZND5J5XEY======"
+	hmacMd5Base64Expected    = "R5Biaidfd2lWOG5aPqe3Jg=="
+)
+
+func TestEncrypt_ByHmacMd5_FromStringToString(t *testing.T) {
+	assert := assert.New(t)
+
+	hexTests := []struct {
+		input1   string // 输入值1
+		input2   string // 输入值2
+		expected string // 期望值
+	}{
+		{"", "", ""},
+		{hmacMd5Input, hmacMd5Key, hmacMd5HexExpected},
+	}
+
+	for index, test := range hexTests {
+		e := Encrypt.FromString(test.input1).ByHmacMd5(test.input2)
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToHexString(), "Current test index is "+strconv.Itoa(index))
+	}
+
+	base32Tests := []struct {
+		input1   string // 输入值1
+		input2   string // 输入值2
+		expected string // 期望值
+	}{
+		{"", "", ""},
+		{hmacMd5Input, hmacMd5Key, hmacMd5Base32Expected},
+	}
+
+	for index, test := range base32Tests {
+		e := Encrypt.FromString(test.input1).ByHmacMd5(test.input2)
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToBase32String(), "Current test index is "+strconv.Itoa(index))
+	}
+
+	base64Tests := []struct {
+		input1   string // 输入值1
+		input2   string // 输入值2
+		expected string // 期望值
+	}{
+		{"", "", ""},
+		{hmacMd5Input, hmacMd5Key, hmacMd5Base64Expected},
+	}
+
+	for index, test := range base64Tests {
+		e := Encrypt.FromString(test.input1).ByHmacMd5(test.input2)
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToBase64String(), "Current test index is "+strconv.Itoa(index))
+	}
+}
+
+func TestEncrypt_ByHmacMd5_FromBytesToBytes(t *testing.T) {
+	assert := assert.New(t)
+
+	hexTests := []struct {
+		input1   []byte // 输入值1
+		input2   []byte // 输入值2
+		expected []byte // 期望值
+	}{
+		{[]byte(""), []byte(""), []byte("")},
+		{[]byte(hmacMd5Input), []byte(hmacMd5Key), []byte(hmacMd5HexExpected)},
+	}
+
+	for index, test := range hexTests {
+		e := Encrypt.FromBytes(test.input1).ByHmacMd5(test.input2)
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToHexBytes(), "Current test index is "+strconv.Itoa(index))
+	}
+
+	base32Tests := []struct {
+		input1   []byte // 输入值1
+		input2   []byte // 输入值2
+		expected []byte // 期望值
+	}{
+		{[]byte(""), []byte(""), []byte("")},
+		{[]byte(hmacMd5Input), []byte(hmacMd5Key), []byte(hmacMd5Base32Expected)},
+	}
+
+	for index, test := range base32Tests {
+		e := Encrypt.FromBytes(test.input1).ByHmacMd5(test.input2)
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToBase32Bytes(), "Current test index is "+strconv.Itoa(index))
+	}
+
+	base64Tests := []struct {
+		input1   []byte // 输入值1
+		input2   []byte // 输入值2
+		expected []byte // 期望值
+	}{
+		{[]byte(""), []byte(""), []byte("")},
+		{[]byte(hmacMd5Input), []byte(hmacMd5Key), []byte(hmacMd5Base64Expected)},
+	}
+
+	for index, test := range base64Tests {
+		e := Encrypt.FromBytes(test.input1).ByHmacMd5(test.input2)
+		assert.Nil(e.Error)
+		assert.Equal(test.expected, e.ToBase64Bytes(), "Current test index is "+strconv.Itoa(index))
+	}
+}
