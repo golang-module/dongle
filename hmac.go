b0baa210ac8d90001a1e9cfcaea0f67a56d120f1
gouguoyin <245629560@qq.com>
2022-12-06 21:54:14 +0800

    Add support for hmac-sha3-224, hmac-sha3-256, hmac-sha-384, hmac-sha3-512, hmac-sha512-224, hmac-sha512-256 encrypt

--- hmac.go
+++ hmac.go
@@ -9,112 +9,155 @@ import (
 	"github.com/tjfoc/gmsm/sm3"
 	"golang.org/x/crypto/md4"
 	"golang.org/x/crypto/ripemd160"
+	"golang.org/x/crypto/sha3"
+	"hash"
 )
 
 // ByHmacMd4 encrypts by hmac with md4.
 // 通过 hmac-md4 加密
-func (e encrypt) ByHmacMd4(key interface{}) encrypt {
+func (e encrypter) ByHmacMd4(key interface{}) encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hash := hmac.New(md4.New, interface2bytes(key))
-	hash.Write(e.src)
-	e.dst = hash.Sum(nil)
+	mac := hmac.New(md4.New, interface2bytes(key))
+	mac.Write(e.src)
+	e.dst = mac.Sum(nil)
 	return e
 }
 
 // ByHmacMd5 encrypts by hmac with md5.
 // 通过 hmac-md5 加密
-func (e encrypt) ByHmacMd5(key interface{}) encrypt {
+func (e encrypter) ByHmacMd5(key interface{}) encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hash := hmac.New(md5.New, interface2bytes(key))
-	hash.Write(e.src)
-	e.dst = hash.Sum(nil)
+	mac := hmac.New(md5.New, interface2bytes(key))
+	mac.Write(e.src)
+	e.dst = mac.Sum(nil)
 	return e
 }
 
 // ByHmacSha1 encrypts by hmac with sha1.
 // 通过 hmac-sha1 加密
-func (e encrypt) ByHmacSha1(key interface{}) encrypt {
+func (e encrypter) ByHmacSha1(key interface{}) encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hash := hmac.New(sha1.New, interface2bytes(key))
-	hash.Write(e.src)
-	e.dst = hash.Sum(nil)
+	mac := hmac.New(sha1.New, interface2bytes(key))
+	mac.Write(e.src)
+	e.dst = mac.Sum(nil)
+	return e
+}
+
+// ByHmacSha3 encrypts by hmac with sha3.
+// 通过 hmac-sha3 加密
+func (e encrypter) ByHmacSha3(key interface{}, digests int) encrypter {
+	if len(e.src) == 0 {
+		return e
+	}
+	var hashFun func() hash.Hash
+	switch digests {
+	case 224:
+		hashFun = sha3.New224
+	case 256:
+		hashFun = sha3.New256
+	case 384:
+		hashFun = sha3.New384
+	case 512:
+		hashFun = sha3.New512
+	default:
+		e.Error = invalidHashDigestsError()
+		return e
+	}
+	mac := hmac.New(hashFun, interface2bytes(key))
+	mac.Write(e.src)
+	e.dst = mac.Sum(nil)
 	return e
 }
 
 // ByHmacSha224 encrypts by hmac with sha224.
 // 通过 hmac-sha224 加密
-func (e encrypt) ByHmacSha224(key interface{}) encrypt {
+func (e encrypter) ByHmacSha224(key interface{}) encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hash := hmac.New(sha256.New224, interface2bytes(key))
-	hash.Write(e.src)
-	e.dst = hash.Sum(nil)
+	mac := hmac.New(sha256.New224, interface2bytes(key))
+	mac.Write(e.src)
+	e.dst = mac.Sum(nil)
 	return e
 }
 
 // ByHmacSha256 encrypts by hmac with sha256.
 // 通过 hmac-sha256 加密
-func (e encrypt) ByHmacSha256(key interface{}) encrypt {
+func (e encrypter) ByHmacSha256(key interface{}) encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hash := hmac.New(sha256.New, interface2bytes(key))
-	hash.Write(e.src)
-	e.dst = hash.Sum(nil)
+	mac := hmac.New(sha256.New, interface2bytes(key))
+	mac.Write(e.src)
+	e.dst = mac.Sum(nil)
 	return e
 }
 
 // ByHmacSha384 encrypts by hmac with sha384.
 // 通过 hmac-sha384 加密
-func (e encrypt) ByHmacSha384(key interface{}) encrypt {
+func (e encrypter) ByHmacSha384(key interface{}) encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hash := hmac.New(sha512.New384, interface2bytes(key))
-	hash.Write(e.src)
-	e.dst = hash.Sum(nil)
+	mac := hmac.New(sha512.New384, interface2bytes(key))
+	mac.Write(e.src)
+	e.dst = mac.Sum(nil)
 	return e
 }
 
 // ByHmacSha512 encrypts by hmac with sha512.
 // 通过 hmac-sha512 加密
-func (e encrypt) ByHmacSha512(key interface{}) encrypt {
+func (e encrypter) ByHmacSha512(key interface{}, digests ...int) encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hash := hmac.New(sha512.New, interface2bytes(key))
-	hash.Write(e.src)
-	e.dst = hash.Sum(nil)
+	var hashFun func() hash.Hash
+	if len(digests) == 0 {
+		digests = []int{512}
+	}
+	switch digests[0] {
+	case 512:
+		hashFun = sha512.New
+	case 224:
+		hashFun = sha512.New512_224
+	case 256:
+		hashFun = sha512.New512_256
+	default:
+		e.Error = invalidHashDigestsError()
+		return e
+	}
+	mac := hmac.New(hashFun, interface2bytes(key))
+	mac.Write(e.src)
+	e.dst = mac.Sum(nil)
 	return e
 }
 
 // ByHmacRipemd160 encrypts by hmac with ripemd160.
 // 通过 hmac-ripemd160 加密
-func (e encrypt) ByHmacRipemd160(key interface{}) encrypt {
+func (e encrypter) ByHmacRipemd160(key interface{}) encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hash := hmac.New(ripemd160.New, interface2bytes(key))
-	hash.Write(e.src)
-	e.dst = hash.Sum(nil)
+	mac := hmac.New(ripemd160.New, interface2bytes(key))
+	mac.Write(e.src)
+	e.dst = mac.Sum(nil)
 	return e
 }
 
 // ByHmacSm3 encrypts by hmac with sm3.
 // 通过 hmac-sm3 加密
-func (e encrypt) ByHmacSm3(key interface{}) encrypt {
+func (e encrypter) ByHmacSm3(key interface{}) encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hash := hmac.New(sm3.New, interface2bytes(key))
-	hash.Write(e.src)
-	e.dst = hash.Sum(nil)
+	mac := hmac.New(sm3.New, interface2bytes(key))
+	mac.Write(e.src)
+	e.dst = mac.Sum(nil)
 	return e
 }
--- hmac_test.go
+++ hmac_test.go
@@ -1,13 +1,13 @@
 package dongle
 
 import (
-	"strconv"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-var hmacTest = []struct {
+var hmacTests = []struct {
 	hash     string
 	input    string
 	key      string
@@ -23,6 +23,18 @@ var hmacTest = []struct {
 	{"sha1", "", "", "", ""},
 	{"sha1", "hello world", "dongle", "91c103ef93ba7420902b0d1bf0903251c94b4a62", "kcED75O6dCCQKw0b8JAyUclLSmI="},
 
+	{"sha3-224", "", "", "", ""},
+	{"sha3-224", "hello world", "dongle", "fb8f061d9d1dddd2f5d3b9064a5e98e3e4b6df27ea93ce67627583ce", "+48GHZ0d3dL107kGSl6Y4+S23yfqk85nYnWDzg=="},
+
+	{"sha3-256", "", "", "", ""},
+	{"sha3-256", "hello world", "dongle", "8193367fde28cf5c460adb449a04b3dd9c184f488bdccbabf0526c54f90c4460", "gZM2f94oz1xGCttEmgSz3ZwYT0iL3Mur8FJsVPkMRGA="},
+
+	{"sha3-384", "", "", "", ""},
+	{"sha3-384", "hello world", "dongle", "3f76f5cda69cada3ee6b33f8458cd498b063075db263dd8b33f2a3992a8804f9569a7c86ffa2b8f0748babeb7a6fc0e7", "P3b1zaacraPuazP4RYzUmLBjB12yY92LM/KjmSqIBPlWmnyG/6K48HSLq+t6b8Dn"},
+
+	{"sha3-512", "", "", "", ""},
+	{"sha3-512", "hello world", "dongle", "a99653d0407d659eccdeed43bb7cccd2e2b05a2c34fd3467c4198cf2ad26a466738513e88839fb55e64eb49df65bc52ed0fec2775bd9e086edd4fb4024add4a2", "qZZT0EB9ZZ7M3u1Du3zM0uKwWiw0/TRnxBmM8q0mpGZzhRPoiDn7VeZOtJ32W8Uu0P7Cd1vZ4Ibt1PtAJK3Uog=="},
+
 	{"sha224", "", "", "", ""},
 	{"sha224", "hello world", "dongle", "e15b9e5a7eccb1f17dc81dc07c909a891936dc3429dc0d940accbcec", "4VueWn7MsfF9yB3AfJCaiRk23DQp3A2UCsy87A=="},
 
@@ -35,6 +47,12 @@ var hmacTest = []struct {
 	{"sha512", "", "", "", ""},
 	{"sha512", "hello world", "dongle", "d971b790bbc2a4ac81062bbffac693c9c234bae176c8faf5e304dbdb153032a826f12353964b4a4fb87abecd2dc237638a630cbad54a6b94b1f6ef5d5e2835d1", "2XG3kLvCpKyBBiu/+saTycI0uuF2yPr14wTb2xUwMqgm8SNTlktKT7h6vs0twjdjimMMutVKa5Sx9u9dXig10Q=="},
 
+	{"sha512-224", "", "", "", ""},
+	{"sha512-224", "hello world", "dongle", "c4145bcc385c29f0e5683cd5450be9deb522d556de3b046a7ffa1eb3", "xBRbzDhcKfDlaDzVRQvp3rUi1VbeOwRqf/oesw=="},
+
+	{"sha512-256", "", "", "", ""},
+	{"sha512-256", "hello world", "dongle", "e99fae71bcb43651ae10e952989eadf897faccb43966ee5122bb1b1d82f7a7c2", "6Z+ucby0NlGuEOlSmJ6t+Jf6zLQ5Zu5RIrsbHYL3p8I="},
+
 	{"ripemd160", "", "", "", ""},
 	{"ripemd160", "hello world", "dongle", "3691ad040e80c43dc6e8ffe9bc6ef3d5bd8786b8", "NpGtBA6AxD3G6P/pvG7z1b2Hhrg="},
 
@@ -43,7 +61,7 @@ var hmacTest = []struct {
 }
 
 func TestHmac_Encrypt_ToString(t *testing.T) {
-	for index, test := range hmacTest {
+	for index, test := range hmacTests {
 		e := Encrypt.FromString(test.input)
 
 		switch test.hash {
@@ -53,6 +71,14 @@ func TestHmac_Encrypt_ToString(t *testing.T) {
 			e = e.ByHmacMd5(test.key)
 		case "sha1":
 			e = e.ByHmacSha1(test.key)
+		case "sha3-224":
+			e = e.ByHmacSha3(test.key, 224)
+		case "sha3-256":
+			e = e.ByHmacSha3(test.key, 256)
+		case "sha3-384":
+			e = e.ByHmacSha3(test.key, 384)
+		case "sha3-512":
+			e = e.ByHmacSha3(test.key, 512)
 		case "sha224":
 			e = e.ByHmacSha224(test.key)
 		case "sha256":
@@ -61,21 +87,27 @@ func TestHmac_Encrypt_ToString(t *testing.T) {
 			e = e.ByHmacSha384(test.key)
 		case "sha512":
 			e = e.ByHmacSha512(test.key)
+		case "sha512-224":
+			e = e.ByHmacSha512(test.key, 224)
+		case "sha512-256":
+			e = e.ByHmacSha512(test.key, 256)
 		case "ripemd160":
 			e = e.ByHmacRipemd160(test.key)
 		case "sm3":
 			e = e.ByHmacSm3(test.key)
 		}
 
-		assert.Nil(t, e.Error)
-
-		assert.Equal(t, test.toHex, e.ToHexString(), "Hex test index is "+strconv.Itoa(index))
-		assert.Equal(t, test.toBase64, e.ToBase64String(), "Base64 test index is "+strconv.Itoa(index))
+		t.Run(fmt.Sprintf(test.hash+"_test_%d", index), func(t *testing.T) {
+			assert.Nil(t, e.Error)
+			assert.Equal(t, fmt.Sprintf("%s", e), e.ToRawString())
+			assert.Equal(t, test.toHex, e.ToHexString())
+			assert.Equal(t, test.toBase64, e.ToBase64String())
+		})
 	}
 }
 
 func TestHmac_Encrypt_ToBytes(t *testing.T) {
-	for index, test := range hmacTest {
+	for index, test := range hmacTests {
 		e := Encrypt.FromBytes([]byte(test.input)).ByHmacMd4([]byte(test.key))
 
 		switch test.hash {
@@ -85,6 +117,14 @@ func TestHmac_Encrypt_ToBytes(t *testing.T) {
 			e = e.ByHmacMd5([]byte(test.key))
 		case "sha1":
 			e = e.ByHmacSha1([]byte(test.key))
+		case "sha3-224":
+			e = e.ByHmacSha3([]byte(test.key), 224)
+		case "sha3-256":
+			e = e.ByHmacSha3([]byte(test.key), 256)
+		case "sha3-384":
+			e = e.ByHmacSha3([]byte(test.key), 384)
+		case "sha3-512":
+			e = e.ByHmacSha3([]byte(test.key), 512)
 		case "sha224":
 			e = e.ByHmacSha224([]byte(test.key))
 		case "sha256":
@@ -93,15 +133,32 @@ func TestHmac_Encrypt_ToBytes(t *testing.T) {
 			e = e.ByHmacSha384([]byte(test.key))
 		case "sha512":
 			e = e.ByHmacSha512([]byte(test.key))
+		case "sha512-224":
+			e = e.ByHmacSha512([]byte(test.key), 224)
+		case "sha512-256":
+			e = e.ByHmacSha512([]byte(test.key), 256)
 		case "ripemd160":
 			e = e.ByHmacRipemd160([]byte(test.key))
 		case "sm3":
 			e = e.ByHmacSm3([]byte(test.key))
 		}
 
-		assert.Nil(t, e.Error)
-
-		assert.Equal(t, []byte(test.toHex), e.ToHexBytes(), "Hex test test index is "+strconv.Itoa(index))
-		assert.Equal(t, []byte(test.toBase64), e.ToBase64Bytes(), "Base64 test index is "+strconv.Itoa(index))
+		t.Run(fmt.Sprintf(test.hash+"_test_%d", index), func(t *testing.T) {
+			assert.Nil(t, e.Error)
+			assert.Equal(t, []byte(test.toHex), e.ToHexBytes())
+			assert.Equal(t, []byte(test.toBase64), e.ToBase64Bytes())
+		})
 	}
+}
+
+func TestHmac_Digests_Error(t *testing.T) {
+	e1 := Encrypt.FromString("hello world").ByHmacSha3("dongle", 100)
+	assert.Equal(t, invalidHashDigestsError(), e1.Error)
+	e2 := Encrypt.FromBytes([]byte("hello world")).ByHmacSha3([]byte("dongle"), 100)
+	assert.Equal(t, invalidHashDigestsError(), e2.Error)
+
+	e3 := Encrypt.FromString("hello world").ByHmacSha512("dongle", 100)
+	assert.Equal(t, invalidHashDigestsError(), e3.Error)
+	e4 := Encrypt.FromBytes([]byte("hello world")).ByHmacSha512([]byte("dongle"), 100)
+	assert.Equal(t, invalidHashDigestsError(), e4.Error)
 }
