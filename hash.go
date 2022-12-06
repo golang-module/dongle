57bd738848a79497be07ed9af05f277b0013f830
gouguoyin <245629560@qq.com>
2022-12-06 21:53:35 +0800

    Add support for sha3-224, sha3-256, sha-384, sha3-512, sha512-224, sha512-256 encrypt

--- hash.go
+++ hash.go
@@ -1,101 +1,201 @@
 package dongle
 
 import (
+	"crypto"
 	"crypto/md5"
 	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/sha512"
 	"golang.org/x/crypto/md4"
 	"golang.org/x/crypto/ripemd160"
+	"golang.org/x/crypto/sha3"
+	"hash"
+)
+
+// hash algorithm constants
+// 哈希算法常量
+const (
+	MD4       = crypto.MD4
+	MD5       = crypto.MD5
+	SHA1      = crypto.SHA1
+	SHA224    = crypto.SHA224
+	SHA256    = crypto.SHA256
+	SHA384    = crypto.SHA384
+	SHA512    = crypto.SHA512
+	RIPEMD160 = crypto.RIPEMD160
+
+	SHA3_224   = crypto.SHA3_224
+	SHA3_256   = crypto.SHA3_256
+	SHA3_384   = crypto.SHA3_384
+	SHA3_512   = crypto.SHA3_512
+	SHA512_224 = crypto.SHA512_224
+	SHA512_256 = crypto.SHA512_256
 )
 
 // ByMd4 encrypts by md4.
 // 通过 md4 加密
-func (e encrypt) ByMd4() encrypt {
+func (e encrypter) ByMd4() encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hash := md4.New()
-	hash.Write(e.src)
-	e.dst = hash.Sum(nil)
+	e.dst = hashEncrypt(e.src, MD4)
 	return e
 }
 
 // ByMd5 encrypts by md5.
 // 通过 md5 加密
-func (e encrypt) ByMd5() encrypt {
+func (e encrypter) ByMd5() encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hash := md5.New()
-	hash.Write(e.src)
-	e.dst = hash.Sum(nil)
+	e.dst = hashEncrypt(e.src, MD5)
 	return e
 }
 
 // BySha1 encrypts by sha1.
 // 通过 sha1 加密
-func (e encrypt) BySha1() encrypt {
+func (e encrypter) BySha1() encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	bytes := sha1.Sum(e.src)
-	e.dst = bytes[:]
+	e.dst = hashEncrypt(e.src, SHA1)
+	return e
+}
+
+// BySha3 encrypts by sha3.
+// 通过 BySha3 加密
+func (e encrypter) BySha3(digests int) encrypter {
+	if len(e.src) == 0 {
+		return e
+	}
+	switch digests {
+	case 224:
+		e.dst = hashEncrypt(e.src, SHA3_224)
+	case 256:
+		e.dst = hashEncrypt(e.src, SHA3_256)
+	case 384:
+		e.dst = hashEncrypt(e.src, SHA3_384)
+	case 512:
+		e.dst = hashEncrypt(e.src, SHA3_512)
+	default:
+		e.Error = invalidHashDigestsError()
+	}
 	return e
 }
 
 // BySha224 encrypts by sha224.
 // 通过 sha224 加密
-func (e encrypt) BySha224() encrypt {
+func (e encrypter) BySha224() encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	bytes := sha256.Sum224(e.src)
-	e.dst = bytes[:]
+	e.dst = hashEncrypt(e.src, SHA224)
 	return e
 }
 
 // BySha256 encrypts by sha256.
 // 通过 sha256 加密
-func (e encrypt) BySha256() encrypt {
+func (e encrypter) BySha256() encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	bytes := sha256.Sum256(e.src)
-	e.dst = bytes[:]
+	e.dst = hashEncrypt(e.src, SHA256)
 	return e
 }
 
 // BySha384 encrypts by sha384.
 // 通过 sha384 加密
-func (e encrypt) BySha384() encrypt {
+func (e encrypter) BySha384() encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	bytes := sha512.Sum384(e.src)
-	e.dst = bytes[:]
+	e.dst = hashEncrypt(e.src, SHA384)
 	return e
 }
 
 // BySha512 encrypts by sha512.
 // 通过 sha512 加密
-func (e encrypt) BySha512() encrypt {
+func (e encrypter) BySha512(digests ...int) encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	bytes := sha512.Sum512(e.src)
-	e.dst = bytes[:]
+	if len(digests) == 0 {
+		e.dst = hashEncrypt(e.src, SHA512)
+		return e
+	}
+	switch digests[0] {
+	case 224:
+		e.dst = hashEncrypt(e.src, SHA512_224)
+	case 256:
+		e.dst = hashEncrypt(e.src, SHA512_256)
+	default:
+		e.Error = invalidHashDigestsError()
+	}
 	return e
 }
 
 // ByRipemd160 encrypts by ripemd160.
 // 通过 ripemd160 加密
-func (e encrypt) ByRipemd160() encrypt {
+func (e encrypter) ByRipemd160() encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hash := ripemd160.New()
-	hash.Write(e.src)
-	e.dst = hash.Sum(nil)
+	e.dst = hashEncrypt(e.src, RIPEMD160)
 	return e
+}
+
+// hashEncrypt hash encrypt src.
+// 对 src 进行哈希加密
+func hashEncrypt(src []byte, algo crypto.Hash) (dst []byte) {
+	var mac hash.Hash
+	switch algo {
+	case MD4:
+		mac = md4.New()
+		mac.Write(src)
+		dst = mac.Sum(nil)
+	case MD5:
+		mac = md5.New()
+		mac.Write(src)
+		dst = mac.Sum(nil)
+	case RIPEMD160:
+		mac = ripemd160.New()
+		mac.Write(src)
+		dst = mac.Sum(nil)
+	case SHA1:
+		bytes := sha1.Sum(src)
+		dst = bytes[:]
+	case SHA3_224:
+		bytes := sha3.Sum224(src)
+		dst = bytes[:]
+	case SHA3_256:
+		bytes := sha3.Sum256(src)
+		dst = bytes[:]
+	case SHA3_384:
+		bytes := sha3.Sum384(src)
+		dst = bytes[:]
+	case SHA3_512:
+		bytes := sha3.Sum512(src)
+		dst = bytes[:]
+	case SHA224:
+		bytes := sha256.Sum224(src)
+		dst = bytes[:]
+	case SHA256:
+		bytes := sha256.Sum256(src)
+		dst = bytes[:]
+	case SHA384:
+		bytes := sha512.Sum384(src)
+		dst = bytes[:]
+	case SHA512:
+		bytes := sha512.Sum512(src)
+		dst = bytes[:]
+	case SHA512_224:
+		mac = sha512.New512_224()
+		mac.Write(src)
+		dst = mac.Sum(nil)
+	case SHA512_256:
+		mac = sha512.New512_256()
+		mac.Write(src)
+		dst = mac.Sum(nil)
+	}
+	return dst
 }
--- hash_test.go
+++ hash_test.go
@@ -1,13 +1,13 @@
 package dongle
 
 import (
-	"strconv"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-var hashTest = []struct {
+var hashTests = []struct {
 	hash     string
 	input    string
 	toHex    string
@@ -22,6 +22,18 @@ var hashTest = []struct {
 	{"sha1", "", "", ""},
 	{"sha1", "hello world", "2aae6c35c94fcfb415dbe95f408b9ce91ee846ed", "Kq5sNclPz7QV2+lfQIuc6R7oRu0="},
 
+	{"sha3-224", "", "", ""},
+	{"sha3-224", "hello world", "dfb7f18c77e928bb56faeb2da27291bd790bc1045cde45f3210bb6c5", "37fxjHfpKLtW+ustonKRvXkLwQRc3kXzIQu2xQ=="},
+
+	{"sha3-256", "", "", ""},
+	{"sha3-256", "hello world", "644bcc7e564373040999aac89e7622f3ca71fba1d972fd94a31c3bfbf24e3938", "ZEvMflZDcwQJmarInnYi88px+6HZcv2Uoxw7+/JOOTg="},
+
+	{"sha3-384", "", "", ""},
+	{"sha3-384", "hello world", "83bff28dde1b1bf5810071c6643c08e5b05bdb836effd70b403ea8ea0a634dc4997eb1053aa3593f590f9c63630dd90b", "g7/yjd4bG/WBAHHGZDwI5bBb24Nu/9cLQD6o6gpjTcSZfrEFOqNZP1kPnGNjDdkL"},
+
+	{"sha3-512", "", "", ""},
+	{"sha3-512", "hello world", "840006653e9ac9e95117a15c915caab81662918e925de9e004f774ff82d7079a40d4d27b1b372657c61d46d470304c88c788b3a4527ad074d1dccbee5dbaa99a", "hAAGZT6ayelRF6FckVyquBZikY6SXengBPd0/4LXB5pA1NJ7GzcmV8YdRtRwMEyIx4izpFJ60HTR3MvuXbqpmg=="},
+
 	{"sha224", "", "", ""},
 	{"sha224", "hello world", "2f05477fc24bb4faefd86517156dafdecec45b8ad3cf2522a563582b", "LwVHf8JLtPrv2GUXFW2v3s7EW4rTzyUipWNYKw=="},
 
@@ -34,21 +46,37 @@ var hashTest = []struct {
 	{"sha512", "", "", ""},
 	{"sha512", "hello world", "309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86d4cd86f989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76f", "MJ7MSJwS1utMxA9QyQLytNDtd+5RGnx6m808qG1M2G+YndNbxf9JlnDaNCVbRbDP2DDoH2Bdz33FVC6TrpzXbw=="},
 
+	{"sha512-224", "", "", ""},
+	{"sha512-224", "hello world", "22e0d52336f64a998085078b05a6e37b26f8120f43bf4db4c43a64ee", "IuDVIzb2SpmAhQeLBabjeyb4Eg9Dv020xDpk7g=="},
+
+	{"sha512-256", "", "", ""},
+	{"sha512-256", "hello world", "0ac561fac838104e3f2e4ad107b4bee3e938bf15f2b15f009ccccd61a913f017", "CsVh+sg4EE4/LkrRB7S+4+k4vxXysV8AnMzNYakT8Bc="},
+
 	{"ripemd160", "", "", ""},
 	{"ripemd160", "hello world", "98c615784ccb5fe5936fbc0cbe9dfdb408d92f0f", "mMYVeEzLX+WTb7wMvp39tAjZLw8="},
 }
 
 func TestHash_Encrypt_ToString(t *testing.T) {
-	for index, test := range hashTest {
+	for index, test := range hashTests {
 		e := Encrypt.FromString(test.input)
 
 		switch test.hash {
+		case "ripemd160":
+			e = e.ByRipemd160()
 		case "md4":
 			e = e.ByMd4()
 		case "md5":
 			e = e.ByMd5()
 		case "sha1":
 			e = e.BySha1()
+		case "sha3-224":
+			e = e.BySha3(224)
+		case "sha3-256":
+			e = e.BySha3(256)
+		case "sha3-384":
+			e = e.BySha3(384)
+		case "sha3-512":
+			e = e.BySha3(512)
 		case "sha224":
 			e = e.BySha224()
 		case "sha256":
@@ -57,28 +85,42 @@ func TestHash_Encrypt_ToString(t *testing.T) {
 			e = e.BySha384()
 		case "sha512":
 			e = e.BySha512()
-		case "ripemd160":
-			e = e.ByRipemd160()
+		case "sha512-224":
+			e = e.BySha512(224)
+		case "sha512-256":
+			e = e.BySha512(256)
 		}
 
-		assert.Nil(t, e.Error)
-
-		assert.Equal(t, test.toHex, e.ToHexString(), "Current test index is "+strconv.Itoa(index))
-		assert.Equal(t, test.toBase64, e.ToBase64String(), "Current test index is "+strconv.Itoa(index))
+		t.Run(fmt.Sprintf(test.hash+"_test_%d", index), func(t *testing.T) {
+			assert.Nil(t, e.Error)
+			assert.Equal(t, fmt.Sprintf("%s", e), e.ToRawString())
+			assert.Equal(t, test.toHex, e.ToHexString())
+			assert.Equal(t, test.toBase64, e.ToBase64String())
+		})
 	}
 }
 
 func TestHash_Encrypt_ToBytes(t *testing.T) {
-	for index, test := range hashTest {
+	for index, test := range hashTests {
 		e := Encrypt.FromBytes([]byte(test.input))
 
 		switch test.hash {
+		case "ripemd160":
+			e = e.ByRipemd160()
 		case "md4":
 			e = e.ByMd4()
 		case "md5":
 			e = e.ByMd5()
 		case "sha1":
 			e = e.BySha1()
+		case "sha3-224":
+			e = e.BySha3(224)
+		case "sha3-256":
+			e = e.BySha3(256)
+		case "sha3-384":
+			e = e.BySha3(384)
+		case "sha3-512":
+			e = e.BySha3(512)
 		case "sha224":
 			e = e.BySha224()
 		case "sha256":
@@ -87,13 +129,28 @@ func TestHash_Encrypt_ToBytes(t *testing.T) {
 			e = e.BySha384()
 		case "sha512":
 			e = e.BySha512()
-		case "ripemd160":
-			e = e.ByRipemd160()
+		case "sha512-224":
+			e = e.BySha512(224)
+		case "sha512-256":
+			e = e.BySha512(256)
 		}
 
-		assert.Nil(t, e.Error)
-
-		assert.Equal(t, []byte(test.toHex), e.ToHexBytes(), "Current test index is "+strconv.Itoa(index))
-		assert.Equal(t, []byte(test.toBase64), e.ToBase64Bytes(), "Current test index is "+strconv.Itoa(index))
+		t.Run(fmt.Sprintf(test.hash+"_test_%d", index), func(t *testing.T) {
+			assert.Nil(t, e.Error)
+			assert.Equal(t, []byte(test.toHex), e.ToHexBytes())
+			assert.Equal(t, []byte(test.toBase64), e.ToBase64Bytes())
+		})
 	}
+}
+
+func TestHash_Digests_Error(t *testing.T) {
+	e1 := Encrypt.FromString("hello world").BySha3(100)
+	assert.Equal(t, invalidHashDigestsError(), e1.Error)
+	e2 := Encrypt.FromBytes([]byte("hello world")).BySha3(100)
+	assert.Equal(t, invalidHashDigestsError(), e2.Error)
+
+	e3 := Encrypt.FromString("hello world").BySha512(100)
+	assert.Equal(t, invalidHashDigestsError(), e3.Error)
+	e4 := Encrypt.FromBytes([]byte("hello world")).BySha512(100)
+	assert.Equal(t, invalidHashDigestsError(), e4.Error)
 }
