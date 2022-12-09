f8bfe96ecf3e9498c4ab1b542f8ae80828649fd4
gouguoyin <245629560@qq.com>
2022-12-09 11:19:22 +0800

    Optimize ToBool() method judgment

--- verify.go
+++ verify.go
@@ -14,17 +14,17 @@ func newVerifier() verifier {
 }
 
 // FromRawString verify signature from raw string.
-// 对未经编码的原始字符串进行验签
-func (v verifier) FromRawString(sign, message string) verifier {
+// 对未经编码的原始字符串签名进行验签
+func (v verifier) FromRawString(signature, message string) verifier {
 	v.src = string2bytes(message)
-	v.sign = string2bytes(sign)
+	v.sign = string2bytes(signature)
 	return v
 }
 
 // FromHexString decrypts from string with hex encoding.
-// 对经过 hex 编码的字符串进行验签
-func (v verifier) FromHexString(sign, message string) verifier {
-	decode := Decode.FromString(sign).ByHex()
+// 对经过 hex 编码的字符串签名进行验签
+func (v verifier) FromHexString(signature, message string) verifier {
+	decode := Decode.FromString(signature).ByHex()
 	if decode.Error != nil {
 		v.Error = invalidDecodingError("hex")
 		return v
@@ -35,9 +35,9 @@ func (v verifier) FromHexString(sign, message string) verifier {
 }
 
 // FromBase64String decrypts from string with base64 encoding.
-// 对经过 base64 编码的字符串进行验签
-func (v verifier) FromBase64String(sign, message string) verifier {
-	decode := Decode.FromString(sign).ByBase64()
+// 对经过 base64 编码的字符串签名进行验签
+func (v verifier) FromBase64String(signature, message string) verifier {
+	decode := Decode.FromString(signature).ByBase64()
 	if decode.Error != nil {
 		v.Error = invalidDecodingError("base64")
 		return v
@@ -48,17 +48,17 @@ func (v verifier) FromBase64String(sign, message string) verifier {
 }
 
 // FromRawBytes verify signature from raw byte slice.
-// 对未经编码的原始字节切片进行验签
-func (v verifier) FromRawBytes(sign, message []byte) verifier {
-	v.sign = sign
+// 对未经编码的原始字节切片签名进行验签
+func (v verifier) FromRawBytes(signature, message []byte) verifier {
+	v.sign = signature
 	v.src = message
 	return v
 }
 
 // FromHexBytes verify from byte slice with hex encoding.
-// 对经过 hex 编码的字节切片进行验签
-func (v verifier) FromHexBytes(sign, message []byte) verifier {
-	decode := Decode.FromBytes(sign).ByHex()
+// 对经过 hex 编码的字节切片签名进行验签
+func (v verifier) FromHexBytes(signature, message []byte) verifier {
+	decode := Decode.FromBytes(signature).ByHex()
 	if decode.Error != nil {
 		v.Error = invalidDecodingError("hex")
 		return v
@@ -69,9 +69,9 @@ func (v verifier) FromHexBytes(sign, message []byte) verifier {
 }
 
 // FromBase64Bytes verify from byte slice with base64 encoding.
-// 对经过 base64 编码的字节切片进行验签
-func (v verifier) FromBase64Bytes(sign, message []byte) verifier {
-	decode := Decode.FromBytes(sign).ByBase64()
+// 对经过 base64 编码的字节切片签名进行验签
+func (v verifier) FromBase64Bytes(signature, message []byte) verifier {
+	decode := Decode.FromBytes(signature).ByBase64()
 	if decode.Error != nil {
 		v.Error = invalidDecodingError("base64")
 		return v
@@ -84,5 +84,8 @@ func (v verifier) FromBase64Bytes(sign, message []byte) verifier {
 // ToBool outputs as bool.
 // 输出布尔值
 func (v verifier) ToBool() bool {
+	if len(v.src) == 0 || len(v.sign) == 0 {
+		return false
+	}
 	return v.Error == nil
 }
