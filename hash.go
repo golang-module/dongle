94ce8cf0e1d096ea6bacc1ec7151f6d936836a36
gouguoyin <245629560@qq.com>
2022-12-13 07:44:09 +0800

    hasher->h

--- hash.go
+++ hash.go
@@ -37,9 +37,9 @@ func (e Encrypter) ByMd2() Encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hasher := md2.New()
-	hasher.Write(e.src)
-	e.dst = hasher.Sum(nil)
+	h := md2.New()
+	h.Write(e.src)
+	e.dst = h.Sum(nil)
 	return e
 }
 
@@ -49,9 +49,9 @@ func (e Encrypter) ByMd4() Encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hasher := md4.New()
-	hasher.Write(e.src)
-	e.dst = hasher.Sum(nil)
+	h := md4.New()
+	h.Write(e.src)
+	e.dst = h.Sum(nil)
 	return e
 }
 
@@ -61,9 +61,9 @@ func (e Encrypter) ByMd5() Encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hasher := md5.New()
-	hasher.Write(e.src)
-	e.dst = hasher.Sum(nil)
+	h := md5.New()
+	h.Write(e.src)
+	e.dst = h.Sum(nil)
 	return e
 }
 
@@ -73,34 +73,34 @@ func (e Encrypter) BySha1() Encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hasher := sha1.New()
-	hasher.Write(e.src)
-	e.dst = hasher.Sum(nil)
+	h := sha1.New()
+	h.Write(e.src)
+	e.dst = h.Sum(nil)
 	return e
 }
 
 // BySha3 encrypts by sha3.
 // 通过 BySha3 加密
 func (e Encrypter) BySha3(size int) Encrypter {
-	var hasher hash.Hash
+	var h hash.Hash
 	if len(e.src) == 0 {
 		return e
 	}
 	switch size {
 	case 224:
-		hasher = sha3.New224()
+		h = sha3.New224()
 	case 256:
-		hasher = sha3.New256()
+		h = sha3.New256()
 	case 384:
-		hasher = sha3.New384()
+		h = sha3.New384()
 	case 512:
-		hasher = sha3.New512()
+		h = sha3.New512()
 	default:
 		e.Error = invalidHashSizeError()
 		return e
 	}
-	hasher.Write(e.src)
-	e.dst = hasher.Sum(nil)
+	h.Write(e.src)
+	e.dst = h.Sum(nil)
 	return e
 }
 
@@ -110,9 +110,9 @@ func (e Encrypter) BySha224() Encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hasher := sha256.New224()
-	hasher.Write(e.src)
-	e.dst = hasher.Sum(nil)
+	h := sha256.New224()
+	h.Write(e.src)
+	e.dst = h.Sum(nil)
 	return e
 }
 
@@ -122,9 +122,9 @@ func (e Encrypter) BySha256() Encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hasher := sha256.New()
-	hasher.Write(e.src)
-	e.dst = hasher.Sum(nil)
+	h := sha256.New()
+	h.Write(e.src)
+	e.dst = h.Sum(nil)
 	return e
 }
 
@@ -134,36 +134,36 @@ func (e Encrypter) BySha384() Encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hasher := sha512.New384()
-	hasher.Write(e.src)
-	e.dst = hasher.Sum(nil)
+	h := sha512.New384()
+	h.Write(e.src)
+	e.dst = h.Sum(nil)
 	return e
 }
 
 // BySha512 encrypts by sha512.
 // 通过 sha512 加密
 func (e Encrypter) BySha512(size ...int) Encrypter {
-	var hasher hash.Hash
+	var h hash.Hash
 	if len(e.src) == 0 {
 		return e
 	}
 	if len(size) == 0 {
-		hasher = sha512.New()
-		hasher.Write(e.src)
-		e.dst = hasher.Sum(nil)
+		h = sha512.New()
+		h.Write(e.src)
+		e.dst = h.Sum(nil)
 		return e
 	}
 	switch size[0] {
 	case 224:
-		hasher = sha512.New512_224()
+		h = sha512.New512_224()
 	case 256:
-		hasher = sha512.New512_256()
+		h = sha512.New512_256()
 	default:
 		e.Error = invalidHashSizeError()
 		return e
 	}
-	hasher.Write(e.src)
-	e.dst = hasher.Sum(nil)
+	h.Write(e.src)
+	e.dst = h.Sum(nil)
 	return e
 }
 
@@ -173,8 +173,8 @@ func (e Encrypter) ByRipemd160() Encrypter {
 	if len(e.src) == 0 {
 		return e
 	}
-	hasher := ripemd160.New()
-	hasher.Write(e.src)
-	e.dst = hasher.Sum(nil)
+	h := ripemd160.New()
+	h.Write(e.src)
+	e.dst = h.Sum(nil)
 	return e
 }
