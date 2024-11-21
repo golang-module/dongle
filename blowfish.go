f541d7fbb7ea3d42f371c680f5071ada212ccd4d
gouguoyin <245629560@qq.com>
2024-11-21 22:22:15 +0800

    Remove errors from errors.go to the current file

--- blowfish.go
+++ blowfish.go
@@ -1,26 +1,47 @@
 package dongle
 
 import (
+	"fmt"
+
 	"golang.org/x/crypto/blowfish"
 )
 
+type BlowfishError struct {
+}
+
+func NewBlowfishError() BlowfishError {
+	return BlowfishError{}
+}
+
+func (e BlowfishError) SrcError() error {
+	return fmt.Errorf("blowfish: invalid src, the src is not full blocks")
+}
+
+func (e BlowfishError) KeyError(key []byte) error {
+	return fmt.Errorf("blowfish: invalid key size %d, the key must from 1 to 56 bytes", len(key))
+}
+
+func (e BlowfishError) IvError(iv []byte) error {
+	return fmt.Errorf("blowfish: invalid iv size %d, the iv size must be 8 bytes", len(iv))
+}
+
 // ByBlowfish encrypts by blowfish.
-// 通过 blowfish 加密
 func (e Encrypter) ByBlowfish(c *Cipher) Encrypter {
 	if len(e.src) == 0 || e.Error != nil {
 		return e
 	}
+	blowfishError := BlowfishError{}
 	block, err := blowfish.NewCipher(c.key)
 	if err != nil {
-		e.Error = invalidBlowfishKeyError()
+		e.Error = blowfishError.KeyError(c.key)
 		return e
 	}
 	if c.mode != ECB && len(c.iv) != block.BlockSize() {
-		e.Error = invalidBlowfishIVError()
+		e.Error = blowfishError.IvError(c.iv)
 		return e
 	}
 	if c.padding == No && len(e.src)%block.BlockSize() != 0 {
-		e.Error = invalidBlowfishSrcError()
+		e.Error = blowfishError.SrcError()
 		return e
 	}
 	e.dst, e.Error = c.Encrypt(e.src, block)
@@ -28,22 +49,22 @@ func (e Encrypter) ByBlowfish(c *Cipher) Encrypter {
 }
 
 // ByBlowfish decrypts by blowfish.
-// 通过 blowfish 解密
 func (d Decrypter) ByBlowfish(c *Cipher) Decrypter {
 	if len(d.src) == 0 || d.Error != nil {
 		return d
 	}
+	blowfishError := NewBlowfishError()
 	block, err := blowfish.NewCipher(c.key)
 	if err != nil {
-		d.Error = invalidBlowfishKeyError()
+		d.Error = blowfishError.KeyError(c.key)
 		return d
 	}
 	if c.mode != ECB && len(c.iv) != block.BlockSize() {
-		d.Error = invalidBlowfishIVError()
+		d.Error = blowfishError.IvError(c.iv)
 		return d
 	}
 	if (c.mode == CBC || c.padding == No) && len(d.src)%block.BlockSize() != 0 {
-		d.Error = invalidBlowfishSrcError()
+		d.Error = blowfishError.SrcError()
 		return d
 	}
 	d.dst, d.Error = c.Decrypt(d.src, block)
